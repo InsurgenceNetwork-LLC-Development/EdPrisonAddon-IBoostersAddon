b559f84b873e564e9ae5f27657f9ac1c433f8bf7
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 16:05:36 -0500

    api jar update

--- src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
+++ src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
@@ -4,11 +4,11 @@ import com.insurgencedev.edprisonaddon.listeners.EdPrisonEventListener;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "EdPrisonAddon", version = "1.0.0", author = "InsurgenceDev", description = "EdPrison Support")
+@IBoostersAddon(name = "EdPrisonAddon", version = "1.0.1", author = "InsurgenceDev", description = "EdPrison Support")
 public class EdPrisonAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
+    public void onAddonReloadAblesStart() {
         registerEvent(new EdPrisonEventListener());
     }
 }
--- src/main/java/com/insurgencedev/edprisonaddon/listeners/EdPrisonEventListener.java
+++ src/main/java/com/insurgencedev/edprisonaddon/listeners/EdPrisonEventListener.java
@@ -5,40 +5,39 @@ import com.edwardbelt.edprison.events.EdPrisonPossibleEnchantTriggerEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
-import org.insurgencedev.insurgenceboosters.models.booster.GlobalBoosterManager;
-import org.insurgencedev.insurgenceboosters.settings.IBoostersPlayerCache;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
 
 public final class EdPrisonEventListener implements Listener {
 
     @EventHandler
     private void onEarn(EdPrisonAddMultiplierCurrency event) {
-        String type = event.getCurrency();
+        final String TYPE = event.getCurrency();
         final String NAMESPACE = "EDP_CURRENCY";
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getUUID()).findActiveBooster(type, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            event.addMultiplier(boosterResult.getBooster().getMultiplier());
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getUUID()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            event.addMultiplier(boosterResult.getBoosterData().getMultiplier());
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(type, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            event.addMultiplier(boosterResult.getBooster().getMultiplier());
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            event.addMultiplier(globalBooster.getMultiplier());
+            return null;
+        }, () -> null);
     }
 
     @EventHandler
     private void onTrigger(EdPrisonPossibleEnchantTriggerEvent event) {
-        String type = event.getEnchant();
+        final String TYPE = event.getEnchant();
         final String NAMESPACE = "EDP_ENCHANT";
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getPlayer()).findActiveBooster(type, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            event.addPercent(event.getPercent() * boosterResult.getBooster().getMultiplier());
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            event.addPercent(event.getPercent() * boosterResult.getBoosterData().getMultiplier());
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(type, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            event.addPercent(event.getPercent() * boosterResult.getBooster().getMultiplier());
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            event.addPercent(event.getPercent() * globalBooster.getMultiplier());
+            return null;
+        }, () -> null);
     }
 }
