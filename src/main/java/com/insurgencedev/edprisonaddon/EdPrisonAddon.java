261fde34d81246ab4cd962bc916da340c09a5181
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-03-13 18:12:52 -0500

    api jar update + version bump

--- src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
+++ src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
@@ -4,7 +4,7 @@ import com.insurgencedev.edprisonaddon.listeners.EdPrisonEventListener;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "EdPrisonAddon", version = "1.0.1", author = "InsurgenceDev", description = "EdPrison Support")
+@IBoostersAddon(name = "EdPrisonAddon", version = "1.0.2", author = "InsurgenceDev", description = "EdPrison Support")
 public class EdPrisonAddon extends InsurgenceBoostersAddon {
 
     @Override
