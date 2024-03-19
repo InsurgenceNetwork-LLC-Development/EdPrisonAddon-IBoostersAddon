284cb4bf8b94c31bd530e1c048dc0bff93d8f6bd
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-03-19 03:19:02 -0500

    include perm boosters

--- src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
+++ src/main/java/com/insurgencedev/edprisonaddon/EdPrisonAddon.java
@@ -3,12 +3,15 @@ package com.insurgencedev.edprisonaddon;
 import com.insurgencedev.edprisonaddon.listeners.EdPrisonEventListener;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EdPrisonAddon", version = "1.0.2", author = "InsurgenceDev", description = "EdPrison Support")
 public class EdPrisonAddon extends InsurgenceBoostersAddon {
 
     @Override
     public void onAddonReloadAblesStart() {
-        registerEvent(new EdPrisonEventListener());
+        if (Common.doesPluginExist("EdPrison")) {
+            registerEvent(new EdPrisonEventListener());
+        }
     }
 }
--- src/main/java/com/insurgencedev/edprisonaddon/listeners/EdPrisonEventListener.java
+++ src/main/java/com/insurgencedev/edprisonaddon/listeners/EdPrisonEventListener.java
@@ -2,10 +2,16 @@ package com.insurgencedev.edprisonaddon.listeners;
 
 import com.edwardbelt.edprison.events.EdPrisonAddMultiplierCurrency;
 import com.edwardbelt.edprison.events.EdPrisonPossibleEnchantTriggerEvent;
+import com.google.common.util.concurrent.AtomicDouble;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
 import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
+import org.insurgencedev.insurgenceboosters.data.PermanentBoosterData;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public final class EdPrisonEventListener implements Listener {
 
@@ -13,14 +19,18 @@ public final class EdPrisonEventListener implements Listener {
     private void onEarn(EdPrisonAddMultiplierCurrency event) {
         final String TYPE = event.getCurrency();
         final String NAMESPACE = "EDP_CURRENCY";
+        UUID uuid = event.getUUID();
+        AtomicDouble totalMulti = new AtomicDouble(getPermMulti(uuid, TYPE, NAMESPACE) + getGlobalPermMulti(TYPE, NAMESPACE));
 
-        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getUUID()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(uuid).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
         if (pResult instanceof BoosterFindResult.Success boosterResult) {
-            event.addMultiplier(boosterResult.getBoosterData().getMultiplier());
+            totalMulti.getAndAdd(boosterResult.getBoosterData().getMultiplier());
+            event.addMultiplier(totalMulti.get());
         }
 
         IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
-            event.addMultiplier(globalBooster.getMultiplier());
+            totalMulti.getAndAdd(globalBooster.getMultiplier());
+            event.addMultiplier(totalMulti.get());
             return null;
         }, () -> null);
     }
@@ -29,15 +39,35 @@ public final class EdPrisonEventListener implements Listener {
     private void onTrigger(EdPrisonPossibleEnchantTriggerEvent event) {
         final String TYPE = event.getEnchant();
         final String NAMESPACE = "EDP_ENCHANT";
+        Player player = event.getPlayer();
+        AtomicDouble totalMulti = new AtomicDouble(getPermMulti(player.getUniqueId(), TYPE, NAMESPACE) + getGlobalPermMulti(TYPE, NAMESPACE));
 
-        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(player).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
         if (pResult instanceof BoosterFindResult.Success boosterResult) {
-            event.addPercent(event.getPercent() * boosterResult.getBoosterData().getMultiplier());
+            totalMulti.getAndAdd(boosterResult.getBoosterData().getMultiplier());
+            event.addPercent(event.getPercent() * totalMulti.get());
         }
 
         IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
-            event.addPercent(event.getPercent() * globalBooster.getMultiplier());
+            totalMulti.getAndAdd(globalBooster.getMultiplier());
+            event.addPercent(event.getPercent() * totalMulti.get());
             return null;
         }, () -> null);
+    }
+
+    private double getPermMulti(UUID uuid, String type, String namespace) {
+        Optional<PermanentBoosterData> foundMulti = Optional.ofNullable(IBoosterAPI.INSTANCE.getCache(uuid).getPermanentBoosts().getPermanentBooster(type, namespace));
+        return foundMulti.map(PermanentBoosterData::getMulti).orElse(0d);
+    }
+
+    private double getGlobalPermMulti(String type, String namespace) {
+        AtomicDouble multi = new AtomicDouble(0d);
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findPermanentBooster(type, namespace, data -> {
+            multi.set(data.getMulti());
+            return null;
+        }, () -> null);
+
+        return multi.get();
     }
 }
